0bedae4dda680f078a56b277b2b2310cec264d75
sakthipriyasangeetha <92252761+sakthipriyasangeetha@users.noreply.github.com>
2021-10-14 12:22:47 +0530

    marks.c

---
+++ marks.c
@@ -0,0 +1,26 @@
+#include<studio.h>
+int main()
+{
+printf("enter the mark");
+int mark=0;
+scanf("%d",&mark);
+if(mark<40)
+{
+printf("Grade F");
+}
+else if(mark>=40 && mark>54)
+{
+printf("Grade D");
+}
+else if(mark>=55 && mark<69)
+{
+printf("Grade C");
+}
+else if(mark>=70 && mark<84)
+{
+printf("Grade B");
+}
+else
+{
+printf(""Grade A");
+}
